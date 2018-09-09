f28427fc4026496768e2db6e90549bf275f07c0e
Yoshi <Yoshi@172.16.67.27>
2018-09-09 12:45:44 +0800

    upload test

--- githubTest/src/githubTest/githubTestMain.java
+++ githubTest/src/githubTest/githubTestMain.java
@@ -5,6 +5,7 @@ public class githubTestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github");
+		System.out.println("Upload test");
 	}
 
 }
