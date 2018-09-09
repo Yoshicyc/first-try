3e80a48835a54de1cbf84deb7b68cf11aac997e2
Yoshi <Yoshi@172.16.67.27>
2018-09-09 13:41:09 +0800

    source code of java Learn project

---
+++ javaLearned/src/myLearnedClase.java
@@ -0,0 +1,39 @@
+
+public class myLearnedClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//1		set auto-fill-in
+//		auto activation triggers for Java ".abc...ABC"
+		System.out.println("successful");
+		
+//2		shortcut key reminder
+//		(1)Ctrl+Space 
+//		说明:内容助理。提供对方法,变量,参数,javadoc等得提示, 
+//		应运在多种场合,总之需要提示的时候可先按此快捷键。 
+//		注:避免输入法的切换设置与此设置冲突
+//		(2)Ctrl+Shift+Space 
+//		说明:变量提示
+//		(3)Ctrl+/ 
+//		说明:添加/消除//注释,在eclipse2.0中,消除注释为Ctrl+\
+//		(4)Ctrl+Shift+/ 
+//		说明:添加/* */注释
+//		(5)Ctrl+Shift+\ 
+//		说明:消除/* */注释
+//		(6)Ctrl+Shift+F 
+//		说明:自动格式化代码
+//		(7)Ctrl+1 
+//		说明:批量修改源代码中的变量名,此外还可用在catch块上.
+//		(8)Ctril+F6 
+//		说明:界面切换
+//		(9)Ctril+Shift+M 
+//		说明:查找所需要得包
+//		(10)Ctril+Shift+O 
+//		说明:自动引入所需要得包
+		
+		
+
+	}
+
+}
